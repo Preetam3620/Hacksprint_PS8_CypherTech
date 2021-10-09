808bc5dd5970b09f2c446cb9565cf9f16a7252af
digvijayingole55 <Sanjay55@@>
2021-10-09 14:27:59 +0530

    question data and pages added fro beginner

--- fluento_app/lib/Screens/Question/models/question.dart
+++ fluento_app/lib/Screens/Question/models/question.dart
@@ -1,4 +1,4 @@
-class Question {
+class McqQuestion {
   String questionType;
   String questionText;
   String imageString;
@@ -6,11 +6,167 @@ class Question {
   String optionB;
   String optionC;
   String optionD;
-  Question({
-    this.questionType, this.questionText, this.imageString, this.optionA, this.optionB, this.optionC, this.optionD
-  });
+  String answer;
+  McqQuestion(
+      {required this.questionType,
+      required this.questionText,
+      required this.imageString,
+      required this.optionA,
+      required this.optionB,
+      required this.optionC,
+      required this.optionD,
+      required this.answer});
 }
 
-List<Question> questions = [
-  Question(questionType: )
-]
+// 3MNI = 3 mcq no image
+// 4MNI = 4 mcq no image
+// 4MI = 4 mcq with image
+List<McqQuestion> hindiTutNumber = [
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the number in the image correctly.',
+      imageString: 'assets/beginnerImages/numbers/t1.png',
+      optionA: 'एक',
+      optionB: 'शून्य',
+      optionC: 'दो',
+      optionD: 'तीन',
+      answer: 'शून्य'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the number in the image correctly.',
+      imageString: 'assets/beginnerImages/numbers/t2.png',
+      optionA: 'आठ',
+      optionB: 'सात',
+      optionC: 'एक',
+      optionD: 'पांच',
+      answer: 'एक'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the number in the image correctly.',
+      imageString: 'assets/beginnerImages/numbers/t3.png',
+      optionA: 'तीन',
+      optionB: 'एक',
+      optionC: 'छह',
+      optionD: 'दो',
+      answer: 'दो'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the number in the image correctly.',
+      imageString: 'assets/beginnerImages/numbers/t4.png',
+      optionA: 'एक',
+      optionB: 'दो',
+      optionC: 'तीन',
+      optionD: 'पांच',
+      answer: 'तीन'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the given number.',
+      imageString: 'assets/beginnerImages/numbers/t5.png',
+      optionA: 'इकतालीस',
+      optionB: 'चौदह',
+      optionC: 'अस्सी-तीन',
+      optionD: 'सत्रह',
+      answer: 'चौदह'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the given number.',
+      imageString: 'assets/beginnerImages/numbers/t6.png',
+      optionA: 'पांच',
+      optionB: 'सैंतीस',
+      optionC: 'पचपन ',
+      optionD: 'पैंतालीस',
+      answer: 'पैंतालीस'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the given number.',
+      imageString: 'assets/beginnerImages/numbers/t7.png',
+      optionA: 'दो',
+      optionB: 'पांच',
+      optionC: 'नौ',
+      optionD: 'सात',
+      answer: 'नौ'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the given number.',
+      imageString: 'assets/beginnerImages/numbers/t8.png',
+      optionA: 'पैंतालीस',
+      optionB: 'चौरासी',
+      optionC: 'उनहत्तर',
+      optionD: 'अड़तालीस',
+      answer: 'उनहत्तर'),
+];
+
+List<McqQuestion> hindiTutAnimals = [
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the number in the image correctly.',
+      imageString: 'assets/beginnerImages/numbers/t1.png',
+      optionA: 'एक',
+      optionB: 'शून्य',
+      optionC: 'दो',
+      optionD: 'तीन',
+      answer: 'शून्य'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the number in the image correctly.',
+      imageString: 'assets/beginnerImages/numbers/t2.png',
+      optionA: 'आठ',
+      optionB: 'सात',
+      optionC: 'एक',
+      optionD: 'पांच',
+      answer: 'एक'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the number in the image correctly.',
+      imageString: 'assets/beginnerImages/numbers/t3.png',
+      optionA: 'तीन',
+      optionB: 'एक',
+      optionC: 'छह',
+      optionD: 'दो',
+      answer: 'दो'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the number in the image correctly.',
+      imageString: 'assets/beginnerImages/numbers/t4.png',
+      optionA: 'एक',
+      optionB: 'दो',
+      optionC: 'तीन',
+      optionD: 'पांच',
+      answer: 'तीन'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the given number.',
+      imageString: 'assets/beginnerImages/numbers/t5.png',
+      optionA: 'इकतालीस',
+      optionB: 'चौदह',
+      optionC: 'अस्सी-तीन',
+      optionD: 'सत्रह',
+      answer: 'चौदह'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the given number.',
+      imageString: 'assets/beginnerImages/numbers/t6.png',
+      optionA: 'पांच',
+      optionB: 'सैंतीस',
+      optionC: 'पचपन ',
+      optionD: 'पैंतालीस',
+      answer: 'पैंतालीस'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the given number.',
+      imageString: 'assets/beginnerImages/numbers/t7.png',
+      optionA: 'दो',
+      optionB: 'पांच',
+      optionC: 'नौ',
+      optionD: 'सात',
+      answer: 'नौ'),
+  McqQuestion(
+      questionType: '4MI',
+      questionText: 'Identify the given number.',
+      imageString: 'assets/beginnerImages/numbers/t8.png',
+      optionA: 'पैंतालीस',
+      optionB: 'चौरासी',
+      optionC: 'उनहत्तर',
+      optionD: 'अड़तालीस',
+      answer: 'उनहत्तर'),
+];
